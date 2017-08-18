1face22f77c6be37f9b03d488ae548f7334cafe9
tangsongyuan <songyuantang@outlook.com>
2017-08-18 15:19:17 +0000

    add authentication

--- app.js
+++ app.js
@@ -2,16 +2,37 @@ var express = require("express");
 var app = express();
 var bodyParser = require("body-parser");
 var mongoose = require("mongoose");
+var passport = require("passport");
+var LocalStrategy = require("passport-local");
 
 var Place = require("./models/place");
 var Comment = require("./models/comment");
+var User = require("./models/user");
 var seedDB = require("./seeds");
 
 mongoose.connect("mongodb://localhost/yelp_places", {useMongoClient: true});
 app.use(bodyParser.urlencoded({extended: true}));
-app.use(express.static("public"));
+app.use(express.static(__dirname + "/public"));
 app.set("view engine", "ejs");
 seedDB();
+
+// passport configuration
+app.use(require("express-session")({
+    secret: "TSY",
+    resave: false,
+    saveUninitialized: false
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+
+passport.use(new LocalStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+app.use(function(req, res, next) {
+    res.locals.currentUser = req.user;
+    next();
+});
 
 app.get("/", function(req, res){
     res.render("homepage");
@@ -19,6 +40,7 @@ app.get("/", function(req, res){
 
 // INDEX - show all places
 app.get("/places", function(req, res){
+    // console.log(req.user);
     // GET ALL PLACES FROM DB
     Place.find({}, function(err, allPlaces){
         if (err) {
@@ -72,7 +94,7 @@ app.get("/places/:id", function(req, res) {
 // COMMENTS ROUTES
 // ====================
 
-app.get("/places/:id/comments/new", function(req, res) {
+app.get("/places/:id/comments/new", isLoggedIn, function(req, res) {
     Place.findById(req.params.id, function(err, findPlace) {
         if (err) {
             console.log(err);
@@ -82,7 +104,7 @@ app.get("/places/:id/comments/new", function(req, res) {
     });
 });
 
-app.post("/places/:id/comments", function(req, res) {
+app.post("/places/:id/comments", isLoggedIn, function(req, res) {
     Place.findById(req.params.id, function(err, findPlace) {
         if (err) {
             console.log(err);
@@ -101,6 +123,56 @@ app.post("/places/:id/comments", function(req, res) {
         }
     }); 
 });
+
+//============
+// AUTH ROUTE
+//============
+
+// show register form
+app.get("/register", function(req, res) {
+    res.render("register");
+});
+
+app.post("/register", function(req, res) {
+    var newUser = new User({username: req.body.username})
+    User.register(newUser, req.body.password, function(err, user) {
+        if (err) {
+            console.log(err);
+            res.render("register");
+        } else {
+            passport.authenticate("local")(req, res, function() {
+                res.redirect("/places");
+            });
+        }
+    });
+});
+
+// show login form
+app.get("/login", function(req, res) {
+    res.render("login");
+});
+
+app.post("/login", passport.authenticate("local", {
+    successRedirect: "/places",
+    failureRedirect: "/login"
+}), function(req, res) {
+});
+
+// logout route
+app.get("/logout", function(req, res) {
+    req.logout();
+    res.redirect("/places");
+});
+
+// middleware
+function isLoggedIn(req, res, next) {
+    if (req.isAuthenticated()) {
+        next();
+    } else {
+        res.redirect("/login");
+    }
+}
+
 
 app.listen(process.env.PORT, process.env.IP, function(){
     console.log("Yelp Camp server get started!");
